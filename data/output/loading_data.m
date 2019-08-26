8e19701ccaf95e8f8d68545657886be705dc7828
rFalque <r.falque@gmail.com>
2019-08-26 14:13:37 +1000

    update data

--- data/output/loading_data.m
+++ data/output/loading_data.m
@@ -1,5 +1,4 @@
 %% Description:
-% load the data
 % 
 % see for documentation:
 % https://au.mathworks.com/help/matlab/ref/graph.html
@@ -9,17 +8,32 @@ clear variables; close all; clc;
 
 %% post processing of the data
 % load data
-maps_id = csvread("map_ids.csv");
 graph_nodes = csvread("graph_nodes.csv");
 graph_edges = csvread("graph_edges.csv");
-ptCloud = pcread("bunny_less_noise.ply");
-pointcloud = ptCloud.Location;
+
+ptCloud_keyframe = pcread("bunny_keypoints.ply");
+ptCloud_with_noise = pcread("bunny_less_noise.ply");
+
+maps_id_keyframe = csvread("map_ids.csv");
+maps_id_for_noisy_ptCloud = zeros(size(ptCloud_with_noise.Location, 1), 1);
+
+% loop for assignement of the maps_id from the ptCloud_with_noise to
+% pt_cloud_keyframe:
+for i = 1:size(ptCloud_with_noise.Location, 1)
+    point = ptCloud_with_noise.Location(i, :);
+    maps_id_for_noisy_ptCloud(i) = maps_id_keyframe(ptCloud_keyframe.findNearestNeighbors(point, 1));
+end
+
+
+pointcloud = ptCloud_keyframe.Location;
 
 % change from 0-index in C++ to 1-index in Matlab:
-maps_id = maps_id +1;
+maps_id_keyframe = maps_id_keyframe +1;
+maps_id_for_noisy_ptCloud = maps_id_for_noisy_ptCloud + 1;
+
 graph_edges = graph_edges +1;
 
-% build skeleton graph
+%% build skeleton graph
 skeleton = graph;
 skeleton = addnode(skeleton,size(graph_nodes, 1));
 for i=1:size(graph_edges, 1)
@@ -31,6 +45,7 @@ subplot 121;
 plot(skeleton);
 title('original graph')
 
+%% build the graph with neighbours
 % set depth first search traversal
 events = {'edgetonew','startnode'};
 T = dfsearch(skeleton,1,events,'Restart',true);
@@ -52,11 +67,11 @@ for i = 1:size(T,1)
             children =  T.Edge(found_children,2);
 
             % generate submap with parent and children
-            submap = pointcloud(maps_id == current, :);
+            submap = pointcloud(maps_id_keyframe == current, :);
             used(current) = used(current)+1;
             for j = 1:size(children,1)
                 submap = [submap;
-                          pointcloud(maps_id == children(j), :)];
+                          pointcloud(maps_id_keyframe == children(j), :)];
                 used(children(j)) = used(children(j))+1;
             end
 
@@ -82,14 +97,14 @@ for i = 1:size(T,1)
                 children =  T.Edge(found_children,2);
                 
                 % generate submap with parent and children
-                submap = pointcloud(maps_id == current, :);
+                submap = pointcloud(maps_id_keyframe == current, :);
                 used(current) = used(current)+1;
                 submap = [submap;
-                          pointcloud(maps_id == parent, :)];
+                          pointcloud(maps_id_keyframe == parent, :)];
                 used(parent) = used(parent)+1;
                 for j = 1:size(children,1)
                     submap = [submap;
-                              pointcloud(maps_id == children(j), :)];
+                              pointcloud(maps_id_keyframe == children(j), :)];
                     used(children(j)) = used(children(j))+1;
                 end
                 
@@ -102,7 +117,7 @@ end
 
 number_of_points = 0;
 for i = 1:size(used, 1)
-    number_of_points = number_of_points + sum(maps_id == i)*used(i);
+    number_of_points = number_of_points + sum(maps_id_keyframe == i)*used(i);
 end
 
 subplot 122;
